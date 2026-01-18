926985d0b86aa6d5cc552e4ad9907202c4cb20bf
Hallam Smith <me@hcssmith.com>
2026-01-18 10:07:03 +0000

    add wildignore to .nvim.lua

--- .nvim.lua
+++ .nvim.lua
@@ -1,1 +1,11 @@
 vim.opt.path:append({ ".", "./**", "Blocks", "Blocks/**", "Services", "Services/**", "X11", "X11/**" })
+vim.opt.wildignore:append({
+  "*/obj/*",
+  "*/obj/**",
+  "*/bin/*",
+  "*/bin/**",
+  "*/.git/*",
+  "*/.git/**",
+  "*/.vs/*",
+  "*/.vs/**",
+})
