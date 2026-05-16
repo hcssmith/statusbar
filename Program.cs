3ead6fe0dbbcf532f9ec603bdee36502ca9d3cb8
Hallam Smith <me@hcssmith.com>
2026-05-16 21:19:37 +0100

    Cleanup

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Services;
-using System.IO;
 
 class Program {
   static async Task Main(string[] args) => await Host.CreateDefaultBuilder(args)
